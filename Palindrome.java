132fa2568cf235dfa987d154c12f5e81bd637138
pledala <39929679+pkledala@users.noreply.github.com>
2023-04-25 10:22:29 +0530

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -1,1 +1,15 @@
-
+class Solution {
+    public boolean isPalindrome(int x) {
+        int originalNumber = x;
+        int reverseNumber = 0;
+        int reminder =0;
+        if(x < 0)
+            return false;
+        while(x > 0) {
+            reminder = x%10;
+            reverseNumber = (reverseNumber*10)+reminder;
+            x=x/10;
+        }
+        return reverseNumber == originalNumber;
+    }
+}
